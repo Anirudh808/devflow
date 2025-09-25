9d6560a877f46c89025746ba42bcbf84ad76c9c2
Anirudh <anirudhmounasamy@gmail.com>
2025-09-25 16:40:31 +0530

    Add Assets and Metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
 const Home = () => {
-  return <h1>Hello, Next.js!</h1>;
+  return (
+    <>
+      <h1 className="h1-bold m-4 font-space-grotesk">
+        Whereas disregard and contempt for human rights have resulted
+      </h1>
+      <h1 className="h1-bold font-inter m-4">
+        Whereas disregard and contempt for human rights have resulted
+      </h1>
+    </>
+  );
 };
 
 export default Home;
